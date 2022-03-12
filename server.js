95e309c866aef90e3372ec568056ad27fbd55c28
Khrystyna Naumova <christina.naumova13@gmail.com>
2022-03-12 16:52:04 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/my-app'));
+app.use(express.static('./dist/my-app'));
 
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/my-app/index.html'));
+app.get('/*', function(req, res) {
+res.sendFile('index.html', {root:'dist/my-app/'});
 });
 
 app.listen(process.env.PORT || 8080);
